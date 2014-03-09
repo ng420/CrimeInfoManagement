7a12ded7da14f4a458d45cf2d18a34e913aed8d6
Nishant Gupta <nish.gupta01@gmail.com>
2014-03-09 18:15:01 +0530

    dbconnect: inline documentation added.

--- crime_project/crime_project/dbconnect.cpp
+++ crime_project/crime_project/dbconnect.cpp
@@ -1,6 +1,9 @@
 #include "StdAfx.h"
 #include "dbconnect.h"
 
+/*	This file is base for all database connections and methods.
+	Constructor establishes the connection to databse server.
+																*/
 
 dbconnect::dbconnect(void)
 {
@@ -18,27 +21,40 @@ dbconnect::dbconnect(void)
 	}
 }
 
+//Search takes query and field, what to search and where to search.
+//Returns dataset of search results.
+//Resistant to SQL injection attacks.
 DataSet^ dbconnect::search(String^ query,String^ field)
 {
-	//MessageBox::Show("Select * from crimetable where "+field+" like \'%"+query+"%\'");
+	//Declarations
 	MySqlDataAdapter^ da;
 	auto ds=gcnew DataSet;
-	if(query->IndexOf('\'')!=-1) {MessageBox::Show("Invalid Query");da=gcnew MySqlDataAdapter("Select * from crimetable where 1=0",con);da->Fill(ds,"std");return ds;}
-	 try{
-	if(field!="Users")
-		da=gcnew MySqlDataAdapter("Select * from crimetable where "+field+" like \'%"+query+"%\'",con);
-	else
-		da=gcnew MySqlDataAdapter("Select * from usertable where `User ID` like \'%"+query+"%\'",con);
 	
-	da->Fill(ds,"std");
+	//Check for valid query 
+	if(query->IndexOf('\'')!=-1) {MessageBox::Show("Invalid Query");da=gcnew MySqlDataAdapter("Select * from crimetable where 1=0",con);da->Fill(ds,"std");return ds;}
+	
+	//There might be problems with sql query, use try
+	try{
+		//Users are in different table
+		//Hence use condition
+		if(field!="Users")
+			da=gcnew MySqlDataAdapter("Select * from crimetable where "+field+" like \'%"+query+"%\'",con);
+		else
+			da=gcnew MySqlDataAdapter("Select * from usertable where `User ID` like \'%"+query+"%\'",con);
+	
+		//fill retrieved results into dataset. 
+		da->Fill(ds,"std");
 	}
 	catch(Exception^ e)
 	{
 		MessageBox::Show("Invalid Query");
 	}
+	//Return the built dataset.
 	return ds;
 }
 
+//viewall()-> View All Records.
+//Returns dataset.
 DataSet^ dbconnect::viewall()
 {
 	auto da=gcnew MySqlDataAdapter("Select * from crimetable",con);
@@ -47,6 +63,8 @@ DataSet^ dbconnect::viewall()
 	return ds;
 }
 
+//View all Users.
+//Returns dataset.
 DataSet^ dbconnect::viewalluser()
 {
 	auto da=gcnew MySqlDataAdapter("Select * from usertable",con);
@@ -55,6 +73,9 @@ DataSet^ dbconnect::viewalluser()
 	return ds;
 }
 
+//Add Records.
+//Takes the fields as input.
+//Runs SQL query using ExecuteNonQuery.
 void dbconnect::add(String^ cat, String^ date,String^ time, String^ loc, String^ des,String^ evi,String^ susp)
 {
 	String^ query="INSERT INTO crimetable (`Category`, `Date`, `Time`, `Location`, `Description`, `Evidence`, `Suspects`) VALUES ( \'"+cat+"\', \'"+date+"\', \'"+time+"\', \'"+loc+"\', \'"+des+"\', \'"+evi+"\', \'"+susp+"\')";
@@ -65,14 +86,20 @@ void dbconnect::add(String^ cat, String^ date,String^ time, String^ loc, String^
 	cmd->ExecuteNonQuery();
 }
 
-
+//Add Records.
+//Takes the fields as input.
+//Runs SQL query using ExecuteNonQuery.
 void dbconnect::add_user(String^ utype,String^ stationid, String^ userid, String^ pass)
 {
 	String^ query="INSERT INTO usertable (`User Type`,`Station ID`, `User ID`, `Password`) VALUES (\'"+utype+"\', \'"+stationid+"\', \'"+userid+"\', \'"+pass+"\')";
+	
+	//Make Query
 	MySqlCommand^ cmd = gcnew MySqlCommand;
 	cmd->Connection = con;
     cmd->CommandText = query;
-	//MessageBox::Show(query);
+	
+	//Try to Add User.
+	//If the User Already Exists, Show Error
 	try{
 	cmd->ExecuteNonQuery();
 	}
@@ -84,7 +111,9 @@ void dbconnect::add_user(String^ utype,String^ stationid, String^ userid, String
 	}
 	MessageBox::Show("User Added Successfully into the database.","Admin", MessageBoxButtons::OK);
 }
-
+//Add Records.
+//Takes the fields as input.
+//Runs SQL query using ExecuteNonQuery.
 void dbconnect::del_user(String^ user_id,String^ stationid,String^ utype)
 {
 	String^ query="DELETE FROM usertable WHERE `User ID`=\'"+user_id+"\' AND `Station ID`=\'"+stationid+"\' AND `User Type`=\'"+utype+"\'" ;
@@ -92,11 +121,14 @@ void dbconnect::del_user(String^ user_id,String^ stationid,String^ utype)
 	MessageBox::Show(query);
 	cmd->Connection = con;
     cmd->CommandText = query;
-	//MessageBox::Show(query);
+	
+	//If the user does not exist, show error.
 	if(cmd->ExecuteNonQuery()) MessageBox::Show("User Deleted Successfully from the database.","Admin", MessageBoxButtons::OK);
 	else MessageBox::Show("Error Deleting Specified User.");
 }
 
+//Verify whether correct User Login received.
+//Uses SQL Result>0 for verification
 bool dbconnect::verify_user(String^ utype, String^ userid, String^ stationid,String^ password)
 {
 	if(userid->IndexOf('\'')!=-1 || stationid->IndexOf('\'')!=-1 || password->IndexOf('\'')!=-1) {MessageBox::Show("Invalid Query");return 0;}
